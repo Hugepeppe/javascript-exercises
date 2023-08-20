5822adf5c31fc45a64d25891db6b5fe3c276b6f0
Vojtech Pfeffer <vojtechpfeffer@gmail.com>
2023-08-20 11:39:41 +0200

    test pass reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textArray = text.split ("");
+    let reverseArray = textArray.reverse ();
+    let reverseText = reverseArray.join ('');
+    return reverseText
 };
 
 // Do not edit below this line
